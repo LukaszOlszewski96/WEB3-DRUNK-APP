fce3a14a17d74b17d9f0c83de5fb540d47b72c54
lukols <lukols.dev@gmail.com>
2022-05-23 10:57:41 +0200

    add styles for details card

--- src/pages/coctail-details/coctail-details.page.tsx
+++ src/pages/coctail-details/coctail-details.page.tsx
@@ -18,8 +18,57 @@ export const CoctailDetails: FC = () => {
             <img src="https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg" />
           </div>
           <div className="coctailDetails__container__card">
-            <h1>Tytuł</h1>
-            <div>Przepis</div>
+            <h1>Mochito</h1>
+            <h3>0xbc4c...f13d</h3>
+            <div className="coctailDetails__container__card__properties">
+              <div className="coctailDetails__container__card__properties__box">
+                <p>Category</p>
+                <p>Category</p>
+              </div>
+              <div className="coctailDetails__container__card__properties__box">
+                <p>Glass to use</p>
+                <p>Glass to use</p>
+              </div>
+
+              <div className="coctailDetails__container__card__properties__box">
+                <p>Type</p>
+                <p>Type</p>
+              </div>
+            </div>
+            <div className="coctailDetails__container__card__preparation">
+              <div className="coctailDetails__container__card__preparation__row">
+                <p className="coctailDetails__container__card__preparation__rowTitle">
+                  Składniki:
+                </p>
+                <ul>
+                  <li className="coctailDetails__container__card__preparation__rowText">
+                    Gin
+                  </li>
+                  <li className="coctailDetails__container__card__preparation__rowText">
+                    Cola
+                  </li>
+                </ul>
+              </div>
+              <div className="coctailDetails__container__card__preparation__row">
+                <p className="coctailDetails__container__card__preparation__rowTitle">
+                  Instrukcja:
+                </p>
+                <p className="coctailDetails__container__card__preparation__rowText">
+                  2022.01.23 | Tytuł wiadomości Faucibus suspendisse justo,
+                  congue viverra molestie ultrices ac. Lorem ipsum dolor sit
+                  amet, consectetur adipiscing elit. Faucibus suspendisse justo,
+                  congue viverra molestie ultrices ac elementum leo. amet,
+                  consectetur adipiscing elit. Faucibus suspendisse justo,
+                  congue viverra molestie ultrices ac elementum leo. amet,
+                  consectetur adipiscing elit. Faucibus suspendisse justo,
+                  congue viverra molestie ultrices ac elementum leo. consectetur
+                  adipiscing elit. Faucibus suspendisse justo, congue viverra
+                  molestie ultrices ac elementum leo. adipiscing elit. Faucibus
+                  suspendisse justo, congue viverra molestie ultrices ac
+                  elementum leo.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
       </div>
